b6dd50e841135d27bf704dec48b4ba5216490d22
Mihail <ternovetchii.2002@mail.ru>
2023-03-07 22:18:38 +0200

    Portfolio
    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+   strings: ["Frontend Developer", "Backend Developer", "FullStack developer"],
+   typeSpeed: 90,
+   backSpeed: 90,
+   backDelay: 1000,
+   loop: true
+});
